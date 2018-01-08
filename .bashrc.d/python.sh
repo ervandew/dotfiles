274eede896992774eb5b1b3f25590fc95381310a
ervandew <ervandew@gmail.com>
2018-01-08 09:06:21 -0700

    don't attempt to source virtualenvwrapper when in a virtualenv
    sourcing virtualenvwrapper, whose python files may be installed in
    python3 site-packages, in a virtualenv using python2, results in an
    error.

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,25 +1,25 @@
 export PYTHONSTARTUP="$HOME/.pystartup"
 
-# for virtualenv wrapper
-if [ "$USER" != "root" -a -f "/usr/bin/virtualenvwrapper.sh" ] ; then
+# for virtualenv wrapper (only source if we aren't in a virtualenv)
+if [ "$USER" != "root" -a -f "/usr/bin/virtualenvwrapper.sh" -a -z "$VIRTUAL_ENV" ] ; then
   export WORKON_HOME=$HOME/.virtualenv
   source /usr/bin/virtualenvwrapper.sh
+fi
 
-  SCREEN=`echo "$TERM" | grep "^screen"`
-  if [ -n "$SCREEN" ] ; then
-    # hack to support starting screen from virtualenv wrapper's postactivate
-    # script, restoring the virtual path which is lost when starting screen.
-    #
-    # Note: VIRTUAL_PATH and VIRTUAL_ENV_SCREEN are set by:
-    #   ~/.virtualenv/postactivate
-    if [ -n "$VIRTUAL_PATH" -a "$VIRTUAL_PATH" != "$PATH" ] ; then
-      export PATH=$VIRTUAL_PATH
-    fi
+SCREEN=$(echo "$TERM" | grep "^screen")
+if [ -n "$SCREEN" ] ; then
+  # hack to support starting screen from virtualenv wrapper's postactivate
+  # script, restoring the virtual path which is lost when starting screen.
+  #
+  # Note: VIRTUAL_PATH and VIRTUAL_ENV_SCREEN are set by:
+  #   ~/.virtualenv/postactivate
+  if [ -n "$VIRTUAL_PATH" -a "$VIRTUAL_PATH" != "$PATH" ] ; then
+    export PATH=$VIRTUAL_PATH
+  fi
 
-    # support re-calling postactivate to ensure any additional setup isn't lost
-    # when starting screen from postactivate
-    if [ -n "$VIRTUAL_ENV_SCREEN" -a -f "$VIRTUAL_ENV/bin/postactivate" ] ; then
-      source $VIRTUAL_ENV/bin/postactivate
-    fi
+  # support re-calling postactivate to ensure any additional setup isn't lost
+  # when starting screen from postactivate
+  if [ -n "$VIRTUAL_ENV_SCREEN" -a -f "$VIRTUAL_ENV/bin/postactivate" ] ; then
+    source $VIRTUAL_ENV/bin/postactivate
   fi
 fi
