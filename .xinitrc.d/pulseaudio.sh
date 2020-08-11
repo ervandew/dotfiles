fc43e0032a04e37e5b05e96a9afd279aa7befead
ervandew <ervandew@gmail.com>
2020-08-11 16:10:31 -0600

    replace w/ comment on how to enable pulseaudio via systemd
    works better than starting via this script since the script version
    wouldn't work on x startup, requiring pulseaudio to be killed and
    restarted. systemd version so far works as expected.

--- .xinitrc.d/pulseaudio.sh
+++ .xinitrc.d/pulseaudio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-if $(which pulseaudio &> /dev/null) ; then
-  pulseaudio --start
-fi
+# enable user level service instead (run as regular user):
+#   $ systemctl --user enable pulseaudio
+
+#if $(which pulseaudio &> /dev/null) ; then
+#  pulseaudio --start
+#fi
