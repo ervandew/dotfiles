fe72a14877b5cfb0e2cfae21083dfcc9ba74a965
ervandew <ervandew@gmail.com>
2021-01-12 09:19:37 -0700

    update eclimd to run using java-11-openjdk

--- .xinitrc.d/eclim.sh
+++ .xinitrc.d/eclim.sh
@@ -2,5 +2,6 @@
 
 if [ -f $HOME/apps/eclipse/eclimd ] ; then
   # start eclim
-  $HOME/apps/eclipse/eclimd -b
+  #$HOME/apps/eclipse/eclimd -b
+  archlinux-java run java-11-openjdk $HOME/apps/eclipse/eclimd -b
 fi
