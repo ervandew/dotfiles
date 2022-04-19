866e304211fe93f20051d3d2da7bb887f9ee8bb8
ervandew <ervandew@gmail.com>
2022-04-19 07:23:44 -0600

    replace monitor script w/ laptop script
    laptop script can toggle the internal laptop monitor and the internal
    keyboard.

--- .xinitrc.d/monitor.sh
+++ .xinitrc.d/monitor.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ -f $HOME/bin/monitor ] ; then
-  # intialize display outputs
-  ~/bin/monitor internal on
-  ~/bin/monitor external on
+if [ -f $HOME/bin/laptop ] ; then
+  # intialize display outputs by forcing a call to turn the internal monitor on
+  # which will detect the external monitor and setup the displays accordingly.
+  ~/bin/laptop monitor on
+
+  # now attempt to turn the internal monitor off. if there is no external
+  # monitor, this will print an error message and do nothing.
+  ~/bin/laptop monitor off
 fi
