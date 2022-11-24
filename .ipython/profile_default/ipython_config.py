e5b0927cb7b4a06ec516c6796f1ba9b1f5d70aeb
ervandew <ervandew@gmail.com>
2022-11-24 15:11:09 -0700

    remove custom style in favor of using ipython style overrides config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -311,26 +311,43 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 ## The name or class of a Pygments style to use for syntax
 #         highlighting:
 #  solarized-dark, monokai, manni, colorful, igor, lovelace, xcode, murphy, paraiso-dark, vim, autumn, stata-light, abap, bw, sas, stata-dark, rrt, pastie, rainbow_dash, native, solarized-light, paraiso-light, perldoc, borland, trac, arduino, default, tango, fruity, vs, emacs, algol_nu, stata, algol, friendly
-
-# Load my own style, from <profile>/styles, programmtically into pygments
-import pkg_resources
-dist = pkg_resources.Distribution(location=os.path.join(
-  os.path.dirname(os.path.abspath(__file__)),
-  'styles',
-))
-dist._ep_map = {'pygments.styles':
-  {
-    'ervandew': pkg_resources.EntryPoint.parse(
-        'ervandew = ervandew:ErvandewStyle',
-        dist=dist,
-    )
-  }
-}
-pkg_resources.working_set.add(dist)
-c.TerminalInteractiveShell.highlighting_style = 'ervandew'
+#c.TerminalInteractiveShell.highlighting_style = ''
 
 ## Override highlighting format for specific tokens
-#c.TerminalInteractiveShell.highlighting_style_overrides = {}
+from pygments import token
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+  token.Whitespace:             '#bbbbbb',
+  token.Comment:                '#999988',
+  token.Comment.Preproc:        'bold noitalic #999999',
+  token.Comment.Special:        'bold #999999',
+  #token.Operator:               'bold',
+  token.String:                 '#bb8844',
+  token.String.Regex:           '#808000',
+  token.Number:                 '#009999',
+  token.Keyword:                '#5884b0',
+  token.Keyword.Type:           '#445588',
+  token.Name.Builtin:           '#999999',
+  token.Name.Function:          '#990000',
+  token.Name.Class:             '#445588',
+  token.Name.Exception:         '#cf6171',
+  token.Name.Namespace:         '#aaaaaa',
+  token.Name.Variable:          '#008080',
+  token.Name.Constant:          '#008080',
+  token.Name.Tag:               '#000080',
+  token.Name.Attribute:         '#008080',
+  token.Name.Entity:            '#800080',
+  token.Generic.Heading:        '#999999',
+  token.Generic.Subheading:     '#aaaaaa',
+  token.Generic.Deleted:        'bg:#ffdddd #000000',
+  token.Generic.Inserted:       'bg:#ddffdd #000000',
+  token.Generic.Error:          '#aa0000',
+  token.Generic.Emph:           'italic',
+  token.Generic.Strong:         'bold',
+  token.Generic.Prompt:         '#555555',
+  token.Generic.Output:         '#888888',
+  token.Generic.Traceback:      '#aa0000',
+  token.Error:                  'bg:#e3d2d2 #a61717',
+}
 
 ## Enable mouse support in the prompt
 #c.TerminalInteractiveShell.mouse_support = False
