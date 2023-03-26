3da059e8ba09275b63444430ef5b35ed9bc00875
ervandew <ervandew@gmail.com>
2023-03-26 13:45:04 -0600

    run eclim in java 17

--- .xinitrc.d/eclim.sh
+++ .xinitrc.d/eclim.sh
@@ -3,5 +3,5 @@
 if [ -f $HOME/apps/eclipse/eclimd ] ; then
   # start eclim
   #$HOME/apps/eclipse/eclimd -b
-  archlinux-java run java-11-openjdk $HOME/apps/eclipse/eclimd -b
+  archlinux-java run java-17-openjdk $HOME/apps/eclipse/eclimd -b
 fi
