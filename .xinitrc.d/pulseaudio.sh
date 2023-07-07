5d011d246dfbe389dc8285dd95922e6ca60a2897
ervandew <ervandew@gmail.com>
2023-07-07 09:03:11 -0600

    use user level pulseaudio systemd unit instead

--- .xinitrc.d/pulseaudio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# enable user level service instead (run as regular user):
-#   $ systemctl --user enable pulseaudio
-
-#if $(which pulseaudio &> /dev/null) ; then
-#  pulseaudio --start
-#fi
