0f177bdde22a16382a92e7cf486b9c07e7366c61
ervandew <ervandew@gmail.com>
2024-06-08 18:32:47 -0600

    remove vim-endwise from nvim config
    nvim-autopairs takes its place

--- .config/nvim/lua/plugins/vim.lua
+++ .config/nvim/lua/plugins/vim.lua
@@ -2,7 +2,6 @@
 return {
   'chrisbra/csv.vim',
   'vim-scripts/python_match.vim',  -- extends % for python
-  'tpope/vim-endwise',             -- self closing lang specific blocks
   'tpope/vim-surround',
 }
 
