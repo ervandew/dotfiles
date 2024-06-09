80adb1f596a3882a50ccce66fd18dd96d4ccefcf
ervandew <ervandew@gmail.com>
2024-06-09 16:45:45 -0600

    add matchup settings

--- .config/nvim/lua/plugins/matchup.lua
+++ .config/nvim/lua/plugins/matchup.lua
@@ -1,3 +1,12 @@
 return {{
   'andymass/vim-matchup',
+  config = function()
+    vim.g.matchup_matchparen_offscreen = {
+      method = 'popup',
+      -- custom highlight, must be defined by colorscheme/autocmd
+      highlight = 'MatchParenOffscreen',
+    }
+    vim.g.matchup_matchparen_deferred = true
+    vim.g.matchup_matchparen_hi_surround_always = true
+  end,
 }}
