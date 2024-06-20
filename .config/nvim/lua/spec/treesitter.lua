4a8deea301e089180129db831e2437ac164c38fe
ervandew <ervandew@gmail.com>
2024-06-20 16:55:46 -0600

    linter appeasment

--- .config/nvim/lua/spec/treesitter.lua
+++ .config/nvim/lua/spec/treesitter.lua
@@ -1,6 +1,7 @@
 return {{
   'nvim-treesitter/nvim-treesitter',
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup({
       ensure_installed = {
         'bash',
