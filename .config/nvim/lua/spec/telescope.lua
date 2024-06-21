5605371ed35d6697587079e2491f67dac790562d
ervandew <ervandew@gmail.com>
2024-06-21 17:07:34 -0600

    add telescope bindings for buffer and builtin

--- .config/nvim/lua/spec/telescope.lua
+++ .config/nvim/lua/spec/telescope.lua
@@ -5,7 +5,9 @@ return {{
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers)
     vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+    vim.keymap.set('n', '<leader>fp', builtin.builtin)
     vim.keymap.set('n', '<leader>fr', function()
       builtin.find_files({
         cwd = vim.fn.expand('%:h'),
