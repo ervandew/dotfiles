d3994768b880a637c00de8a033ac40a9bc7e4f96
ervandew <ervandew@gmail.com>
2024-06-27 20:56:42 -0600

    replace spec/local with eclim, vimscript

--- .config/nvim/lua/spec/eclim.lua
+++ .config/nvim/lua/spec/eclim.lua
@@ -1,26 +1,4 @@
--- local plugins
 return {
-  {
-    dir = vim.fn.stdpath('config') .. '/vimscript',
-    config = function()
-      -- diff
-      vim.keymap.set('ca', 'dn', 'DiffNextChange')
-      vim.keymap.set('ca', 'dp', 'DiffPrevChange')
-
-      -- indentdetect
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = '*',
-        callback = function()
-          if vim.fn.exists(':IndentDetect') == 2 then
-            vim.cmd.IndentDetect()
-          end
-        end
-      })
-
-      -- ranger
-      vim.keymap.set('n', '<leader>/', ':Ranger<cr>', { silent = true })
-    end
-  },
   {
     dir = '~/.vim/bundle/eclim',
     init = function()
