0b4a54d326c1559e872dfa37500b092e4b7e5e71
ervandew <ervandew@gmail.com>
2024-06-27 19:28:24 -0600

    ensure current diagnostic is set on correct buffer

--- .config/nvim/lua/spec/local.lua
+++ .config/nvim/lua/spec/local.lua
@@ -31,6 +31,8 @@ return {
         pattern = '*.py',
         callback = function()
           vim.schedule(function()
+            local bufnr = vim.api.nvim_get_current_buf()
+            local winid = vim.api.nvim_get_current_win()
             local diagnostic = nil
             local max_severity = nil
             if vim.fn['eclim#project#problems#IsProblemsList']() then
@@ -38,18 +40,20 @@ return {
                 e = vim.diagnostic.severity.ERROR,
                 w = vim.diagnostic.severity.WARN,
               }
-              for _, problem in ipairs(vim.fn.getloclist(0)) do
-                local severity = severities[problem['type']]
-                if severity and (not max_severity or severity < max_severity) then
-                  max_severity = severity
-                  diagnostic = problem
+              for _, problem in ipairs(vim.fn.getloclist(winid)) do
+                if problem['bufnr'] == bufnr then
+                  local severity = severities[problem['type']]
+                  if severity and (not max_severity or severity < max_severity) then
+                    max_severity = severity
+                    diagnostic = problem
+                  end
                 end
               end
             end
             if diagnostic ~= nil then
               diagnostic.severity = max_severity
             end
-            vim.b.diagnostic = diagnostic
+            vim.b[bufnr].diagnostic = diagnostic
             vim.o.statusline=vim.o.statusline
           end)
         end
