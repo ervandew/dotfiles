701e5c3ba3da36dc5b12e96172e12b8a5eb428cb
ervandew <ervandew@gmail.com>
2024-07-15 18:23:57 -0600

    update default action in telescope when opening files

--- .config/nvim/lua/spec/telescope.lua
+++ .config/nvim/lua/spec/telescope.lua
@@ -4,22 +4,51 @@ return {{
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files)
-    vim.keymap.set('n', '<leader>fb', builtin.buffers)
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep)
-    vim.keymap.set('n', '<leader>fp', builtin.builtin)
-    vim.keymap.set('n', '<leader>fr', function()
-      builtin.find_files({
-        cwd = vim.fn.expand('%:h'),
-        opts = {},
-      })
-    end)
-
     local action_state = require("telescope.actions.state")
     local actions = require('telescope.actions')
     local finders = require('telescope.finders')
     local pickers = require('telescope.pickers')
 
+    -- change select_default when opening files
+    local attach_mappings_file = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        local picker = action_state.get_current_picker(prompt_bufnr)
+        actions.close(prompt_bufnr)
+
+        local selection = unpack(action_state.get_selected_entry())
+        local cmd = 'split'
+        if vim.fn.expand('%') == '' and
+           not vim.o.modified and
+           vim.fn.line('$') == 1 and
+           vim.fn.getline(1) == ''
+        then
+          cmd = 'edit'
+        end
+        if picker.cwd then
+          selection = picker.cwd .. '/' .. selection
+        end
+
+        vim.cmd(cmd .. ' ' .. selection)
+      end)
+      return true
+    end
+
+    vim.keymap.set('n', '<leader>ff', function()
+      builtin.find_files({ attach_mappings = attach_mappings_file })
+    end)
+    vim.keymap.set('n', '<leader>fg', function()
+      builtin.live_grep({ attach_mappings = attach_mappings_file })
+    end)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers)
+    vim.keymap.set('n', '<leader>fp', builtin.builtin)
+    vim.keymap.set('n', '<leader>fr', function()
+      builtin.find_files({
+        attach_mappings = attach_mappings_file,
+        cwd = vim.fn.expand('%:h'),
+      })
+    end)
+
+    -- action to change the cwd up a level
     local cd_up = function(prompt_bufnr)
       local line = action_state.get_current_line()
       local cwd = action_state.get_current_picker(prompt_bufnr).cwd
@@ -27,10 +56,12 @@ return {{
         cwd = vim.fn.getcwd()
       end
       builtin.find_files({
+        attach_mappings = attach_mappings_file,
         cwd = vim.fn.fnamemodify(cwd, ':h'),
         default_text = line,
       })
     end
+
     require('telescope').setup({ ---@diagnostic disable-line: undefined-field
       defaults = {
         sorting_strategy = 'ascending',
@@ -46,22 +77,20 @@ return {{
           i = {
             ['<tab>'] = actions.move_selection_next,
             ['<s-tab>'] = actions.move_selection_previous,
-            ['<c-s>'] = actions.select_horizontal,
           },
           n = {
             ['<c-c>'] = actions.close,
             ['<space>'] = actions.toggle_selection,
             ['<tab>'] = actions.move_selection_next,
             ['<s-tab>'] = actions.move_selection_previous,
-            ['<c-s>'] = actions.select_horizontal,
             ['<bs>'] = cd_up,
           },
         }
       },
     })
 
-    -- window finder {{{
-    vim.keymap.set('n', '<leader>p', function()
+    -- window picker {{{
+    vim.keymap.set('n', '<leader>fw', function()
       local bufnames = {}
       local name_to_winnr = {}
       local common_path = nil
@@ -98,7 +127,6 @@ return {{
             actions.close(prompt_bufnr)
             local selection = unpack(action_state.get_selected_entry())
             local winnr = name_to_winnr[selection]
-            vim.print({'sel:', selection, 'winnr:', winnr})
             vim.cmd(winnr .. 'winc w')
           end)
           return true
