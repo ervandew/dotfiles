1fed3c8fda92319f6f4b6f57817704e482928ea4
ervandew <ervandew@gmail.com>
2024-07-22 15:08:56 -0600

    allow fzf to search hidden files excluding .git

--- .config/bash/rc.d/fzf.sh
+++ .config/bash/rc.d/fzf.sh
@@ -2,9 +2,9 @@ if $(which fzf &> /dev/null) ; then
   eval "$(fzf --bash)"
 
   # let rg handle ignoring files (.gitignore, etc)
-  export FZF_DEFAULT_COMMAND="rg --files"
+  export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
   # ditto for command line path completion
   _fzf_compgen_path() {
-    rg --files "$1"
+    rg --files --hidden --glob '!.git' "$1"
   }
 fi
