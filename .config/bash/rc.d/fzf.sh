de8bb68c16fd72116b07da281923527fdb8b047c
ervandew <ervandew@gmail.com>
2024-08-01 15:43:51 -0600

    add custom fzf bindings + enable preview

--- .config/bash/rc.d/fzf.sh
+++ .config/bash/rc.d/fzf.sh
@@ -1,6 +1,13 @@
 if $(which fzf &> /dev/null) ; then
   eval "$(fzf --bash)"
 
+  # use tab/s-tab to scroll through results
+  # add preview w/ ctrl-d/u for scrolling
+  export FZF_DEFAULT_OPTS="
+    --bind=tab:down,shift-tab:up,ctrl-d:preview-page-down,ctrl-u:preview-page-up
+    --preview 'bat --color=always --theme=base16 {}'
+  "
+
   # let rg handle ignoring files (.gitignore, etc)
   export FZF_DEFAULT_COMMAND="rg --files --hidden --glob '!.git'"
   # ditto for command line path completion
