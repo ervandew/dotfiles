056d01326e094c2968a47c4351787f905492bcb9
ervandew <ervandew@gmail.com>
2024-08-11 16:35:15 -0600

    set NotebookDir before plugin loads

--- .config/nvim/lua/spec/notebook.lua
+++ .config/nvim/lua/spec/notebook.lua
@@ -1,9 +1,7 @@
 return {{
   'ervandew/notebook',
   dir = '~/projects/vim/notebook',
-  config = function()
+  init = function()
     vim.g.NotebookDir = vim.fn.expand('~/notebook/')
   end
 }}
-
--- vim:fdm=marker
