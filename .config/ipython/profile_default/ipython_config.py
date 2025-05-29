eee7be8c54e5553f7308192ce539fe8e19dd02d2
ervandew <ervandew@gmail.com>
2025-05-29 16:42:31 -0600

    simplify ipython prompts

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -1,5 +1,11 @@
 import sys
 
+import IPython.core.ultratb
+import IPython.terminal.prompts
+import IPython.terminal.interactiveshell
+import prompt_toolkit.styles
+from pygments.token import Token
+
 ## A list of dotted module names of IPython extensions to load.
 c.InteractiveShellApp.extensions = ['autoreload']
 
@@ -18,24 +24,20 @@ c.TerminalInteractiveShell.editing_mode = 'vi'
 c.TerminalInteractiveShell.emacs_bindings_in_vi_insert_mode = False
 
 ## Override highlighting format for specific tokens
-from IPython.terminal import prompts
-from pygments import token
 c.TerminalInteractiveShell.highlighting_style_overrides = {
   # pygments tokens: https://pygments.org/docs/tokens/
-  token.Keyword:                '#666666',
-  token.Name.Builtin:           '#999999',
-  token.Name.Exception:         '#aaaaaa',
-  token.Name.Namespace:         '#aaaaaa',
-  token.Number:                 '#875f87',
-  token.Operator.Word:          '#875fd7',
-  token.String.Regex:           '#708090',
-  token.String:                 '#708090',
+  Token.Keyword:                '#666666',
+  Token.Name.Builtin:           '#999999',
+  Token.Name.Exception:         '#aaaaaa',
+  Token.Name.Namespace:         '#aaaaaa',
+  Token.Number:                 '#875f87',
+  Token.Operator.Word:          '#875fd7',
+  Token.String.Regex:           '#708090',
+  Token.String:                 '#708090',
 
   # the ipython prompt colors
-  prompts.Token.Prompt:         '#808080',
-  prompts.Token.PromptNum:      '#8787ff',
-  prompts.Token.OutPrompt:      '#c0c0c0',
-  prompts.Token.OutPromptNum:   '#8787ff',
+  Token.Prompt:                 '#8787ff',
+  Token.Prompt.Version:         '#808080',
 
   # ipython specific highlights: search for "class:" in ipython/prompt_toolkit
   # prompt_toolkit completion menu colors (requires monkey patch below)
@@ -47,37 +49,34 @@ c.TerminalInteractiveShell.highlighting_style_overrides = {
   'completion-menu.multi-column-meta': 'bg:#333333 #cccccc',
 
   # bracket/paren match
+  'matching-bracket.cursor': 'bg:#333333 #cf9ebe', # insert mode only
   'matching-bracket.other': 'bg:#333333 #cf9ebe',
 }
 
 # choose a better color combo for highlighting lines in a stack trace
 # (default is near unreadable)
-import IPython.core.ultratb
 IPython.core.ultratb.VerboseTB.tb_highlight = 'bg:#333333'
 
-prompt_py_version = '(py %s)' % '{0.major}.{0.minor}'.format(sys.version_info)
-class Prompt(prompts.Prompts):
+prompt_py_version = 'py %s ' % '{0.major}.{0.minor}'.format(sys.version_info)
+class Prompt(IPython.terminal.prompts.Prompts):
   def in_prompt_tokens(self):
-    tokens = super().in_prompt_tokens()
-    tokens.insert(0, (prompts.Token.Prompt, prompt_py_version))
-    return tokens
+    return [
+      (Token.Prompt.Version, prompt_py_version),
+      (Token.Prompt, '> '),
+    ]
 
   def out_prompt_tokens(self):
-    # added just for allignment
-    tokens = super().out_prompt_tokens()
-    padding = 0
-    for token in self.in_prompt_tokens()[:-3]:
-      padding += len(token[1])
-    tokens.insert(0, (prompts.Token.OutPrompt, ' ' * padding))
-    return tokens
+    padding = sum(len(t[1]) for t in self.in_prompt_tokens()[:-1])
+    return [
+      (Token.OutPrompt, ' ' * padding),
+      (Token.OutPrompt, '  '),
+    ]
 
 # Class used to generate Prompt token for prompt_toolkit
 c.TerminalInteractiveShell.prompts_class = Prompt
 
 # hack to allow prompt_toolkit completion styles to be set by
 # highlighting_style_overrides above
-import IPython.terminal.interactiveshell
-import prompt_toolkit.styles
 def _style_from_pygments_dict(pygments_dict):
    pygments_style = []
    for token, style in pygments_dict.items():
